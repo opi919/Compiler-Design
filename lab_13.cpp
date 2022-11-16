cdfe3c64646efdccc076ca4bc2c73f12bfe8b48f
opi919 <khandokermomen919@gmail.com>
2022-11-16 22:22:55 +0600

    first and follow complete

--- lab_13.cpp
+++ lab_13.cpp
@@ -9,12 +9,80 @@ using namespace std;
 int np;
 char ts[10];
 string first[10], follow[10];
+int visited[10][10];
 
 struct grammer
 {
     string p;
     string prod; // production
 } g[10];
+
+void follow_set(int i)
+{
+    for (int j = 0; j < np; j++)
+    {
+        int pos = g[j].prod.find(g[i].p);
+        if (pos != string::npos) // g[i].p find successfully in g[j].prod
+        {
+            string temp = g[j].prod;
+            if (temp[pos + 1] == '\'')
+                pos++;
+            if (temp[pos + 1] == '\0' || temp[pos + 1] == '|') // g[i].p is last character of g[j].prod
+            {
+                if (g[j].p != g[i].p)
+                {
+                    if (!follow[j].empty())
+                        follow[i] += follow[j];
+                }
+            }
+            else if (temp[pos + 1] >= 'A' && temp[pos + 1] <= 'Z') //containing non terminal value after g[i].p in g[j].pord
+            {
+                string non_terminal = "";
+                non_terminal += temp[pos + 1];
+                if (temp[pos + 2] == '\'')
+                {
+                    non_terminal += temp[pos + 2];
+                    pos++;
+                }
+                // finding the grammar which contains non_terminal
+                for (int k = 0; k < np; k++)
+                {
+                    if (g[k].p == non_terminal)
+                    {
+                        if (!first[k].empty() && visited[i][k] != 1) //if first of non_terminal is not empty
+                        {
+                            visited[i][k] = 1;
+                            for (int l = 0; l < first[k].length(); l++)
+                            {
+                                if (first[k][l] == 'e')  // checking for epsilon
+                                {
+                                    pos++;
+                                    // cout << g[j].p <<" "<< g[k].p << endl;
+                                    // cout<<temp[pos + 1]<<endl;
+                                    if (temp[pos + 1] == '\0' || temp[pos + 1] == '|')
+                                    {
+                                        if (g[j].p != g[k].p)
+                                        {
+                                            if (!follow[j].empty())
+                                                follow[i] += follow[j];
+                                        }
+                                    }
+                                }
+                                else
+                                    follow[i] += first[k][l];    // adding first of non_terminal to follow of g[i].p without epsilon
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                follow[i] += temp[pos + 1];
+                return;
+            }
+        }
+    }
+}
 
 void first_set(int i)
 {
@@ -38,7 +106,6 @@ void first_set(int i)
                 else
                 {
                     first[i] += first[j];
-                    
                 }
                 break;
             }
@@ -73,6 +140,7 @@ int main()
                 g[i].prod += ts[j];
         }
     }
+    // first started here
     // find first set
     for (int i = np - 1; i >= 0; i--)
     {
@@ -84,5 +152,21 @@ int main()
     for (int i = 0; i < np; i++)
     {
         cout << g[i].p << " -> {" << first[i] << "}" << endl;
+    }
+
+    // follow started here
+    // find follow set
+    for (int i = 0; i < np; i++)
+    {
+        if (i == 0)
+            follow[i] += "$";
+        follow_set(i);
+    }
+
+    // print follow set
+    cout << "Follow Set: " << endl;
+    for (int i = 0; i < np; i++)
+    {
+        cout << g[i].p << " -> {" << follow[i] << "}" << endl;
     }
 }
